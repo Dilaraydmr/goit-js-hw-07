ebc25c39368758475f5b363d75ef7a969c232c8a
dilar <dilaraaydemir1@gmail.com>
2025-02-17 12:40:38 +0300

    msg

--- js/task-2.js
+++ js/task-2.js
@@ -22,5 +22,14 @@ const images = [
   {
     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
     alt: "Lighthouse Coast Sea",
-  }
+  },
 ];
+const gallery = document.querySelector(`.gallery`);
+images.forEach((image) => {
+  const galleryitem = document.createElement(`li`);
+  const img = document.createElement(`img`);
+  img.setAttribute(`src`, image.url);
+  img.setAttribute(`alt`, image.alt);
+  galleryitem.appendChild(img);
+  gallery.appendChild(galleryitem);
+});
